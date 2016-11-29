4e409911c73069f09ca63bcbd6deab8e891eb9e8
Alex Kwon <alexykwon@gmail.com>
2016-11-28 23:29:43 -0800

    add filter by name class and level

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,19 @@
 var spellControllers = angular.module('spellControllers', ['ngAnimate', 'ngSanitize']);
 
+spellControllers.filter('to_trusted', ['$sce', function($sce){
+        return function(text) {
+            return $sce.trustAsHtml(text);
+        };
+    }])
 spellControllers.controller('ListController', function ($scope, $http) {
+
+	Object.defineProperty($scope, "queryFilter", {
+      get: function() {
+          var out = {};
+          out[$scope.queryBy || "$"] = $scope.query;
+          return out;
+      	}
+  	});
 
 	$http.get("js/data.json").then(function(response) {
 		$scope.spells = response.data;
