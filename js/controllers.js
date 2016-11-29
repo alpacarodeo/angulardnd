f3dfce78cef25a3f140fcbd41546e81d001ebb84
Alex Kwon <alexykwon@gmail.com>
2016-11-28 19:02:36 -0800

    change css

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-var spellControllers = angular.module('spellControllers', []);
+var spellControllers = angular.module('spellControllers', ['ngAnimate', 'ngSanitize']);
 
 spellControllers.controller('ListController', function ($scope, $http) {
 
@@ -9,11 +9,26 @@ spellControllers.controller('ListController', function ($scope, $http) {
 
 });
 
-spellControllers.controller('DetailsController', function ($scope, $http, $routeParams) {
+spellControllers.controller('DetailsController', function ($scope, $http, $routeParams, $sce) {
 
 	$http.get("js/data.json").then(function(response) {
 		$scope.spells = response.data;
 		$scope.whichItem = $routeParams.itemId;
+		$scope.detailDesc = $scope.spells[$scope.whichItem].desc;
+
+
+		if($routeParams.itemId > 0 ) {
+			$scope.prevItem = Number($routeParams.itemId) - 1;
+		} else {
+			$scope.prevItem = $scope.spells.length - 1;
+		}
+
+				if($routeParams.itemId < $scope.spells.length - 1 ) {
+			$scope.nextItem = Number($routeParams.itemId) +1;
+		} else {
+			$scope.nextItem = 0;
+		}
+
 	});
 
 });
